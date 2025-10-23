2ccd3687eccef588f9b6e4e4cda91b1555727536
Raman Mishra <ramanmishra25432543@gmail.com>
2025-10-23 19:10:37 +0530

    Updated Home.js to use deployed backend URL

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -53,7 +53,7 @@ const Home = () => {
     setPrediction(null);
 
     try {
-      const response = await fetch('http://127.0.0.1:5000/predict', {
+      const response = await fetch('https://houseprice-predictor-p3rk.onrender.com', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
